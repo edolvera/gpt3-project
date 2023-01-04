dbea793bb8e8c93553640c95a67dfc9a4412bd74
eolvera <esau.olverahernandez@gmail.com>
2023-01-04 16:54:57 -0600

    Containers route created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Article, Brand, CTA, Feature, Navbar } from './components'
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+
 const App = () => {
   return (
     <div>
