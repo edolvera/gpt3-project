1fe7e91dafbeb9c48338ad663b0ee1c54229bd4b
eolvera <esau.olverahernandez@gmail.com>
2023-01-05 10:07:08 -0600

    Added asstes, Navbar Structure

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3line, RiCLoseLin } from 'react-icons'
+import logo from '../../assets/logo.svg'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3_navbar'>
+      <div className='gpt3_navbar-links'>
+        <div className='gpt3_navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
   )
 }
 
