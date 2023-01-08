3d096dfe91a25b049629ea6f0a01253bd66c79fc
eolvera <esau.olverahernandez@gmail.com>
2023-01-08 15:30:48 -0600

    CTA section complete

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gpt3_cta'>
+      <div className='gpt3_cta-content'>
+        <p>Request early access to get started</p>
+        <h3>Register today and start exploring the endless possibilities</h3>
+      </div>
+      <div className='gpt3_cta-button'>
+        <button>Get started</button>
+      </div>
+    </div>
   )
 }
 
