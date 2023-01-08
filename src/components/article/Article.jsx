f64093a9fec53b91ad9f9157967ec25341556ad2
eolvera <esau.olverahernandez@gmail.com>
2023-01-08 16:58:52 -0600

    Blog section created, Article component done

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({imgURL, date, title}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3_blog-container_article'>
+      <div className='gpt3_blog-container_article-image'>
+        <img src={imgURL} alt='blog' />
+      </div>
+      <div className='gpt3_blog-container_article-content'>
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read full article</p>
+      </div>
+    </div>
   )
 }
 
